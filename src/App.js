478e71cfbb7781973383c7918401eeeba1eebdc5
Bitee-cd <calebduniya45@gmail.com>
2022-02-18 21:43:20 +0100

    tailwind css installation files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
 import * as React from "react";
 import './App.css';
-import Landing from "./Components/Landing";
-import Login from "./Components/Login";
+
+
+import HomePage from "./Components/pages/HomePage/HomePage";
+import Login from "./Components/pages/LoginPage/Login";
 
 function App() {
   return (
    
     <div className="App">
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="login" element={<Login />} />
       </Routes>
       </div>
--- src/Components/pages/LoginPage/Login.js
+++ src/Components/pages/LoginPage/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 function Login() {
-  return <div>
-      Login page
-  </div>;
+  return (<div className="login">
+      <h1>Login</h1>
+      <div className="buttons">
+        <button>google</button>
+        <button>facebook</button>
+      </div>
+      <div className="middleSection">
+        -OR-
+      </div>
+      <form>
+        <div >
+          <input type="email" placeholder='Email Address' />
+        </div>
+        <div >
+          <input type="password" placeholder='Password' />
+        </div>
+        <div>
+          <input type="checkbox" name="" id="" />
+          Remember me
+        </div>
+        <div className="">
+          <button>Log in</button>
+        </div>
+      </form>
+      <div>
+        <p>Don't have an account yet? Sign Up</p>
+        <p>Forgot your password</p>
+      </div>
+  </div>)
 }
 
 export default Login;
