1e0c3fd732534d8a986311b28be8e114ac7d3a59
Bitee-cd <calebduniya45@gmail.com>
2022-02-19 02:43:48 +0100

    tailwind corrections

---
+++ src/Components/UI/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './NavBar.module.css';
+
+function NavBar() {
+  return (
+    <div >
+        <button className={classes.navbar}>Navbar</button>
+    </div>
+  )
+}
+
+export default NavBar
--- src/Components/pages/LoginPage/Login.js
+++ src/Components/pages/LoginPage/Login.js
@@ -2,33 +2,10 @@ import React from 'react';
 
 function Login() {
   return (<div className="login">
-      <h1>Login</h1>
-      <div className="buttons">
-        <button>google</button>
-        <button>facebook</button>
-      </div>
-      <div className="middleSection">
-        -OR-
-      </div>
-      <form>
-        <div >
-          <input type="email" placeholder='Email Address' />
-        </div>
-        <div >
-          <input type="password" placeholder='Password' />
-        </div>
-        <div>
-          <input type="checkbox" name="" id="" />
-          Remember me
-        </div>
-        <div className="">
-          <button>Log in</button>
-        </div>
-      </form>
-      <div>
-        <p>Don't have an account yet? Sign Up</p>
-        <p>Forgot your password</p>
-      </div>
+
+
+      <button>click me</button>
+     <h1 className="5xl"></h1>
   </div>)
 }
 
