6370dc2e1f7d09456f4ed402dd8325073c493fe0
Bitee-cd <calebduniya45@gmail.com>
2022-02-20 01:32:28 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { Routes, Route, Link } from "react-router-dom";
 import * as React from "react";
-import HomePage from "./Components/pages/HomePage/HomePage";
 import Login from "./Components/pages/LoginPage/Login";
 import classes from "./App.module.css";
+import LandingPage from "./Components/pages/LandingPage/LandingPage";
 
 
 function App() {
   return (
    
     <div className="App">
-   
-      <p className={classes.app}>try this</p>
-
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="login" element={<Login />} />
       </Routes>
       </div>
